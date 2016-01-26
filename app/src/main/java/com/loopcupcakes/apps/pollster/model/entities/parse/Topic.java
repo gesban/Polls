f88518e7138f10e75578348ba85d60da56b29080
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-26 17:41:41 -0500

    Add topic entity

---
+++ app/src/main/java/com/loopcupcakes/apps/pollster/model/entities/parse/Topic.java
@@ -0,0 +1,55 @@
+package com.loopcupcakes.apps.pollster.model.entities.parse;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by evin on 1/26/16.
+ */
+@ParseClassName("Topic")
+public class Topic extends ParseObject{
+
+    Topic(){
+
+    }
+
+    public String getName(){
+        return getString("name");
+    }
+
+    public void setName(String name){
+        put("name", name);
+    }
+
+    public String getDescription(){
+        return getString("description");
+    }
+
+    public void setDescription(String description){
+        put("description", description);
+    }
+
+    public String getDelimiter(){
+        return getString("delimiter");
+    }
+
+    public void setDelimiter(String delimiter){
+        put("delimiter", delimiter);
+    }
+
+    public Integer getYear(){
+        return getInt("year");
+    }
+
+    public void setYear(int year){
+        put("year", year);
+    }
+
+    public Integer getPriority(){
+        return getInt("priority");
+    }
+
+    public void setPriority(int priority){
+        put("priority", priority);
+    }
+}
