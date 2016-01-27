7d4a20c0d1595d4f51e98d0a30f6db235b1f9488
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-26 21:36:45 -0500

    Ping local storage first on retrieveTopics

--- app/src/main/java/com/loopcupcakes/apps/polls/viewmodel/MainVM.java
+++ app/src/main/java/com/loopcupcakes/apps/polls/viewmodel/MainVM.java
@@ -2,13 +2,12 @@ package com.loopcupcakes.apps.polls.viewmodel;
 
 
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
@@ -44,18 +43,18 @@ public class MainVM {
     private NavigationView mNavigationView;
     private FrameLayout mMainFrameLayout;
 
-    public MainVM(MainActivity mainActivity){
+    public MainVM(MainActivity mainActivity) {
         mMainActivity = mainActivity;
         mParseVM = new ParseVM(mMainActivity);
 
         mTopics = new ArrayList<>();
     }
 
-    public void initializeThirdPartyLibraries(){
+    public void initializeThirdPartyLibraries() {
         mParseVM.initializeParse();
     }
 
-    public void initializeLayouts(){
+    public void initializeLayouts() {
 
         mDrawerLayout = (DrawerLayout) mMainActivity.findViewById(R.id.a_main_drawer);
         mNavigationView = (NavigationView) mMainActivity.findViewById(R.id.a_main_nav);
@@ -68,7 +67,7 @@ public class MainVM {
 
     private void loadFragment(Constants.FRAGMENT_TYPE fragment_type) {
         Fragment fragment;
-        switch (fragment_type){
+        switch (fragment_type) {
             case HOME:
                 fragment = new HomeFragment();
                 break;
@@ -79,6 +78,7 @@ public class MainVM {
     }
 
     private void configureActionBar() {
+        // TODO: 1/26/16 Add transparent ActionBar
         Toolbar toolbar = (Toolbar) mMainActivity.findViewById(R.id.a_main_toolbar);
 
         mMainActivity.setSupportActionBar(toolbar);
@@ -129,26 +129,45 @@ public class MainVM {
         };
     }
 
-    private void retrieveTopics(){
+    private void retrieveTopics() {
         // TODO: 1/26/16 Update loading TextView if no connection
         // TODO: 1/26/16 Query local datastore first
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Topic");
+        query.orderByAscending("priority");
+        query.fromLocalDatastore();
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null && objects != null && objects.size() > 0) {
+                    updateTopics(objects);
+                    loadFragment(Constants.FRAGMENT_TYPE.HOME);
+                } else {
+                    retrieveTopicsOnline();
+                }
+            }
+        });
+    }
+
+    private void retrieveTopicsOnline() {
+        //// TODO: 1/26/16 Update localstore on connection
         ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Topic");
         query.orderByAscending("priority");
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null){
+                if (e == null) {
                     updateTopics(objects);
                     loadFragment(Constants.FRAGMENT_TYPE.HOME);
-                }else {
-                    Log.d(TAG_, e.getMessage());
+                } else {
+                    Snackbar snackbar = Snackbar.make(mMainFrameLayout, R.string.main_vm_error_internet, Snackbar.LENGTH_LONG);
+                    snackbar.show();
                 }
             }
         });
     }
 
     private void updateTopics(List<ParseObject> objects) {
-        for (ParseObject object : objects){
+        for (ParseObject object : objects) {
             mTopics.add((Topic) object);
         }
         HomeFragment.mTopicAdapter.notifyDataSetChanged();
