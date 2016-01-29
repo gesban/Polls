e63df7612ffdc9666a717388871f7f68ac7fa760
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-29 01:39:13 -0500

    Add magic to AboutFragment

--- app/src/main/java/com/loopcupcakes/apps/polls/view/fragments/AboutFragment.java
+++ app/src/main/java/com/loopcupcakes/apps/polls/view/fragments/AboutFragment.java
@@ -1,12 +1,14 @@
 package com.loopcupcakes.apps.polls.view.fragments;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.loopcupcakes.apps.polls.R;
 
@@ -26,6 +28,23 @@ public class AboutFragment extends DialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            dialog.getWindow().setLayout(width, height);
+        }
     }
 
 }
