d70d69c0521467e1cc08aa6121b9b285607f8741
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-30 19:53:04 -0500

    Change chart color order

--- app/src/main/java/com/loopcupcakes/apps/polls/viewmodel/utils/ColorMagic.java
+++ app/src/main/java/com/loopcupcakes/apps/polls/viewmodel/utils/ColorMagic.java
@@ -8,20 +8,21 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 public class ColorMagic {
 
     private static final int[] mColors = new int[] {
-            ColorTemplate.VORDIPLOM_COLORS[0],
-            ColorTemplate.VORDIPLOM_COLORS[1],
-            ColorTemplate.VORDIPLOM_COLORS[2],
-            ColorTemplate.VORDIPLOM_COLORS[3],
-            ColorTemplate.VORDIPLOM_COLORS[4],
             ColorTemplate.JOYFUL_COLORS[0],
             ColorTemplate.JOYFUL_COLORS[1],
             ColorTemplate.JOYFUL_COLORS[2],
             ColorTemplate.JOYFUL_COLORS[3],
             ColorTemplate.JOYFUL_COLORS[4],
+            ColorTemplate.VORDIPLOM_COLORS[0],
+            ColorTemplate.VORDIPLOM_COLORS[1],
+            ColorTemplate.VORDIPLOM_COLORS[2],
+            ColorTemplate.VORDIPLOM_COLORS[3],
+            ColorTemplate.VORDIPLOM_COLORS[4],
             ColorTemplate.COLORFUL_COLORS[1],
             ColorTemplate.COLORFUL_COLORS[2],
             ColorTemplate.COLORFUL_COLORS[3],
             ColorTemplate.COLORFUL_COLORS[4],
+            ColorTemplate.COLORFUL_COLORS[0],
     };
 
     public static int createColor(int i) {
