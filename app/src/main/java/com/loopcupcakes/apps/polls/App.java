09c81498d31264e82d72f5e1179ce3410b4c96e3
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-30 22:42:50 -0500

    Optimize imports

--- app/src/main/java/com/loopcupcakes/apps/polls/App.java
+++ app/src/main/java/com/loopcupcakes/apps/polls/App.java
@@ -3,9 +3,6 @@ package com.loopcupcakes.apps.polls;
 import android.app.Application;
 
 import com.loopcupcakes.apps.polls.viewmodel.ParseVM;
-import com.parse.Parse;
-import com.parse.ParseACL;
-import com.parse.ParseUser;
 
 /**
  * Created by evin on 1/30/16.
@@ -20,8 +17,6 @@ public class App extends Application {
         parseVM.initializeParse();
 
         //        Fabric.with(mMainActivity, new Crashlytics());
-
-
     }
 
 }
