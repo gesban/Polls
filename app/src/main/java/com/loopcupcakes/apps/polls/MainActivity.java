e0439e1807c11af7f690304d72e693f3f9cc316e
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-01 15:56:41 -0500

    Fix todo messages

--- app/src/main/java/com/loopcupcakes/apps/polls/MainActivity.java
+++ app/src/main/java/com/loopcupcakes/apps/polls/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = Constants.MainActivityTAG_;
     // TODO: 1/28/16 Implement Analytics
     // TODO: 1/28/16 Add image to topics
-    // TODO: 1/29/16 Add watch an ad support
+    // TODO: 1/29/16 Add ad-watch support
     // TODO: 1/29/16 Add search feature
     // TODO: 1/30/16 Add share button on candidates list
     // TODO: 1/30/16 Add https://developers.google.com/app-indexing/android/app
-    // TODO: 1/30/16 Add onReceive push logic
+    // TODO: 1/30/16 Add onReceive push notification logic
     // TODO: 1/31/16 Ask to be added to https://github.com/wasabeef/recyclerview-animators app-list
-    // TODO: 1/31/16 Add animations when an Activity is open
+    // TODO: 1/31/16 Add animations when an Activity is opened
     // TODO: 1/31/16 Add app invites?
 
     private MainVM mMainVM;
