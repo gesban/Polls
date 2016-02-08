1aaae0145e99aa11e935d0b0d96746381a56a4ef
Evin1- <edwin.hdz.12.0@gmail.com>
2016-02-08 18:00:36 -0500

    Add RandomBehaviorTest Espresso logic

---
+++ app/src/androidTest/java/com/loopcupcakes/apps/polls/RandomBehaviorTest.java
@@ -0,0 +1,46 @@
+package com.loopcupcakes.apps.polls;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Random;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by evin on 2/8/16.
+ */
+@RunWith(AndroidJUnit4.class)
+public class RandomBehaviorTest {
+
+    private static final String TAG = "RandomBehaviorTestTAG_";
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void DoesMainListShowRandom() {
+        Random ran = new Random();
+        RecyclerView recyclerView = (RecyclerView) mActivityRule.getActivity().findViewById(R.id.a_main_recycler);
+
+        int n = (recyclerView == null)
+                ? 0
+                : recyclerView.getAdapter().getItemCount();
+        int x = ran.nextInt(n);
+
+        Log.d(TAG, "DoesMainListShowRandom: " + x);
+
+        onView(withId(R.id.a_main_recycler))
+                .perform(RecyclerViewActions
+                        .actionOnItemAtPosition(x, click()));
+    }
+
+}
